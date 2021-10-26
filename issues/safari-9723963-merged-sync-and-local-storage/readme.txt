739d2c70d64a2dd8dcaaf6a69717200eda48ab50
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2021-10-26 14:31:51 +0200

    Add safari-9723963-merged-sync-and-local-storage

---
+++ issues/safari-9723963-merged-sync-and-local-storage/readme.txt
@@ -0,0 +1,23 @@
+Apple Feedback assistant report:
+https://feedbackassistant.apple.com/feedback/9723963
+
+
+Once running the extension, the background page logs the following:
+{
+  test_local: true
+  test_sync: true
+}
+
+{
+  test_local: true
+  test_sync: true
+}
+
+While it should log this instead:
+{
+  test_local: true
+}
+
+{
+  test_sync: true
+}
---
+++ issues/safari-9723963-merged-sync-and-local-storage/sample-extension/background.js
@@ -0,0 +1,18 @@
+/* global browser */
+'use strict';
+
+(async function () {
+  await browser.storage.sync.set({
+    test_sync: true
+  });
+
+  await browser.storage.local.set({
+    test_local: true
+  });
+
+  const storedLocal = await browser.storage.local.get();
+  console.log(storedLocal);
+
+  const storedSynced = await browser.storage.local.get();
+  console.log(storedSynced);
+})();
