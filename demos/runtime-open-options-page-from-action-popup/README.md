172023517713b737c51eed1f702499db531bdd4a
Carlos Jeurissen <1038267+carlosjeurissen@users.noreply.github.com>
2025-04-01 17:15:06 +0200

    Add runtime-open-options-page-from-action-popup

---
+++ demos/runtime-open-options-page-from-action-popup/README.md
@@ -0,0 +1,4 @@
+Safari 18.4 crashes when calling browser.runtime.openOptionsPage() from an action popup.
+
+WebKit bug report:
+TBD
---
+++ demos/runtime-open-options-page-from-action-popup/src/popup.js
@@ -0,0 +1,5 @@
+'use strict';
+
+document.querySelector('button').addEventListener('click', function () {
+  chrome.runtime.openOptionsPage();
+});
