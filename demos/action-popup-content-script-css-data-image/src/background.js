62b9d3d9c8b70dc61562a88196c590f47866def6
Carlos Jeurissen <1038267+carlosjeurissen@users.noreply.github.com>
2026-04-08 15:31:49 +0200

    Add action-popup-content-script-css-data-image

---
+++ demos/action-popup-content-script-css-data-image/src/background.js
@@ -0,0 +1,8 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(() => {});
+chrome.runtime.onStartup.addListener(() => {});
+chrome.action.openPopup();
+
+/* trigger potential host permission dialogs in safari */
+fetch('https://en.wikipedia.org/');
