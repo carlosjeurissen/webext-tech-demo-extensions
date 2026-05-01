c3d2e99a0c62f28f42649235a5fdeec29899e97a
Carlos Jeurissen <1038267+carlosjeurissen@users.noreply.github.com>
2026-05-01 20:25:33 +0200

    Add additional protocols to runtime-setuninstallurl

--- demos/runtime-setuninstallurl/src/main.js
+++ demos/runtime-setuninstallurl/src/main.js
@@ -3,12 +3,31 @@
 const resultsBodyEl = document.getElementById('results');
 
 const variations = [
-  { label: 'Standard URL', value: 'https://example.com' },
-  { label: 'File Protocol', value: 'file:///tmp/test.html' },
-  { label: 'Extension Protocol', value: chrome.runtime.getURL('main.html') },
+  { label: 'https://example.com/', value: 'https://example.com/' },
+  { label: 'http://example.com/', value: 'http://example.com/' },
   { label: 'Empty String', value: '' },
   { label: 'Null', value: null },
   { label: 'Undefined', value: undefined },
+  { label: 'Extension Protocol', value: chrome.runtime.getURL('main.html') },
+  { label: 'ws://example.com/', value: 'ws://example.com/' },
+  { label: 'wss://example.com/', value: 'wss://example.com/' },
+  { label: 'ftp://example.com/', value: 'ftp://example.com/' },
+  { label: 'blob://example.com/', value: 'blob://example.com/' },
+  { label: 'file:///tmp/test.html', value: 'file:///tmp/test.html' },
+  { label: 'data:image', value: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==' },
+  { label: 'javascript:alert(\'Hello!\')', value: 'javascript:alert(\'Hello!\')' },
+  { label: 'chrome-extension:', value: 'chrome-extension://aowiejfoiwej/main.html' },
+  { label: 'moz-extension:', value: 'moz-extension://aowiejfoiwej/main.html' },
+  { label: 'safari-web-extension:', value: 'safari-web-extension://aowiejfoiwej/main.html' },
+  { label: 'orion-extension:', value: 'orion-extension://aowiejfoiwej/main.html' },
+  { label: 'mailto:name@example.com', value: 'mailto:name@example.com' },
+  { label: 'resource://gre/', value: 'resource://gre/' },
+  { label: 'about:blank', value: 'about:blank' },
+  { label: 'about:config', value: 'about:config' },
+  { label: 'chrome://settings', value: 'chrome://settings' },
+  { label: 'tel:+15551234567', value: 'tel:+15551234567' },
+  { label: 'urn:isbn:0451450523', value: 'urn:isbn:0451450523' },
+  { label: 'view-source:https://example.com', value: 'view-source:https://example.com' },
 ];
 
 variations.forEach((item) => {
