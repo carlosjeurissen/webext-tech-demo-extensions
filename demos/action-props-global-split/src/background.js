0c53868bf165f5472f0cd4e5ca7cb21074217508
Carlos Jeurissen <1038267+carlosjeurissen@users.noreply.github.com>
2026-05-30 15:50:26 +0200

    Update action-props-global-split demo to work in Safari

--- demos/action-props-global-split/src/background.js
+++ demos/action-props-global-split/src/background.js
@@ -1,22 +1,37 @@
 'use strict';
 
-function setActionProps () {
-  const inPrivate = chrome.extension.inIncognitoContext;
+function getContextType () {
+  return new Promise(function (resolve, reject) {
+    chrome.windows.getCurrent((result) => {
+      const inPrivate = result && result.incognito;
+      if (typeof inPrivate === 'boolean') {
+        resolve(inPrivate);
+      } else {
+        reject(new Error('failed'));
+      }
+    });
+  }).catch(() => {
+    return Boolean(chrome.extension.inIncognitoContext);
+  });
+}
 
-  chrome.action.setIcon({
-    path: inPrivate ? 'icon.png' : 'override.png',
-  });
-  chrome.action.setTitle({
-    title: inPrivate ? 'private' : 'non private',
-  });
-  chrome.action.setBadgeText({
-    text: inPrivate ? 'P' : 'O',
-  });
-  chrome.action.setBadgeBackgroundColor({
-    color: inPrivate ? 'purple' : 'green',
-  });
-  chrome.action.setBadgeTextColor({
-    color: inPrivate ? 'pink' : 'orange',
+function setActionProps () {
+  getContextType().then((inPrivate) => {
+    chrome.action.setIcon({
+      path: inPrivate ? 'icon.png' : 'override.png',
+    });
+    chrome.action.setTitle({
+      title: inPrivate ? 'private' : 'non private',
+    });
+    chrome.action.setBadgeText({
+      text: inPrivate ? 'P' : 'O',
+    });
+    chrome.action.setBadgeBackgroundColor({
+      color: inPrivate ? 'purple' : 'green',
+    });
+    chrome.action.setBadgeTextColor({
+      color: inPrivate ? 'pink' : 'orange',
+    });
   });
 }
 
